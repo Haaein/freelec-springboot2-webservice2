f8eda2667250976d1818f83fc6a610c76d8ab94a
Raa <say0049@naver.com>
2022-12-24 17:50:47 +0900

    코드 임시추가
    gitignore 적용 확인용 임시코드 커밋

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -9,5 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        int i; //코드 임시 추가
     }
 }
